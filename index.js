8e3fc1a8c60b04e4c1fad8fc1a2761655211fabe
Matt Boggie <mboggie@mac.com>
2016-04-07 19:56:43 -0400

    lots of debugging, getting some inconsistent responses now.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var alexa = require('alexa-app');
 var Levenshtein = require('levenshtein');
 var omdb = require('omdb');
+var fs = require('fs');
 
 var app = new alexa.app();
-
 var knowledge = JSON.parse(fs.readFileSync('knowledge.json', 'utf8'));
 
 app.launch(function(request,response) {
@@ -13,28 +13,31 @@ app.launch(function(request,response) {
 	response.shouldEndSession (false, "What would you like to know?  For examples, say help.  To leave, say exit.");
 });
 
+var names = []
+for (p in knowledge.plays) {
+    names.push(knowledge.plays[p].title);
+}
+
 app.dictionary = {
-    "movie_names": SampleMovies.titles,
+    "movie_names": names,
     "genres": ["Action","Adult","Adventure","Animation","Biography","Comedy","Crime","Documentary","Drama","Family","Fantasy","Film-Noir","History","Horror","Music","Musical","Mystery","News","Romance","Sci-Fi","Short","Sport","Thriller","War","Western"]
 };
 
-app.intent('StartIntent'),
+app.intent('StartIntent',
     {
         "slots": {},
         "utterances":[
             "What should I watch",
-            "What's good"
-            "What's on"
+            "What's good",
+            "What's on",
             "I'm bored"
         ]
     },
     function (request, response) {
         console.log("[StartIntent]");
-        response.say("How about we start with what you like. Give me an idea of a show or movie you really enjoyed.");
-        if (response.session ('open_session') === 'true')
-        {
-            response.shouldEndSession (false);
-        }
+        response.say("How about we start with what you like. Give me an example of a show or movie you really enjoyed.");
+        response.shouldEndSession (false);
+        response.send();
     }
 );
 
@@ -42,16 +45,15 @@ app.intent('FavoriteIntent',
     {
         "slots": {"TITLE":"LITERAL"},
         "utterances": [
-            "I {really |}{liked|loved|love|like} {the movie |the show |}{movie_names|TITLE}",
+            "I {really |}{liked |loved |love |like }{the movie |the show |}{movie_names|TITLE}",
             "How about {the movie |the show |}{movie_names|TITLE}",
-            "What {show|movie|film|program} is {similar|most like|comparable to|like}{movie_names|TITLE}",
+            "What {show|movie|film|program} is {similar |most like |comparable to |like }{movie_names|TITLE}",
             "{What is|What's} something {that is|that's} like {movie_names|TITLE}"
         ]
     },
     function (request, response) {
         console.log ("[FavoriteIntent]");
         filter_movie (response, request.slot('TITLE'));
-        return false;
     }
 );
 
@@ -67,9 +69,8 @@ app.intent('GenreIntent',
     function (request, response) {
         console.log ("[GenreIntent]");
         filter_genre (response, request.slot('GENRE'));
-        return false;
     }
-});
+);
 
 app.intent('HelpIntent', 
     {
@@ -105,16 +106,15 @@ app.intent('HelpIntent',
 	            response.say("What's something that's like  " + moviename);
                 break;
         }
-        if (response.session ('open_session') === 'true')
-        {
-            response.shouldEndSession (false);
-        }
+
+        response.shouldEndSession (false);
+
         response.send ();
     }
 );
 
 function filter_movie(response, title){
-    var filterlist = retrieveWatchingRecs();
+    var filterlist = retrieveWatchingRecs(response);
     var winninglist = [];
     var genres = [];
 
@@ -131,18 +131,15 @@ function filter_movie(response, title){
     }
     else
     {
-        saveFilterList(filterlist);
+        saveFilterList(filterlist, response);
         response.say ("Sorry, but I don't have anything right now that's like " + title + ". What else do you like?")
-        if (response.session ('open_session') === 'true')
-        {
-            response.shouldEndSession (false);
-        }
+        response.shouldEndSession (false);
         response.send ();
     }
 }
 
 function filter_genre(response, genre){
-    var filterlist = retrieveWatchingRecs();
+    var filterlist = retrieveWatchingRecs(response);
     var winninglist = [];
     var genres = [];
     genres.push({"name":genre});
@@ -154,25 +151,23 @@ function filter_genre(response, genre){
     }
     else
     {
-        saveFilterList(filterlist);
+        saveFilterList(filterlist, response);
         response.say ("Sorry, but I don't have anything right now in " + genre + ". What else do you like?")
-        if (response.session ('open_session') === 'true')
-        {
-            response.shouldEndSession (false);
-        }
+        response.shouldEndSession (false);
         response.send ();
     }
 }
 
 function narrowByAttributes(attributes, shows){ 
-// likes == attributes about a show or movie (As array). Shows is an array of shows
-
+// attributes == all attributes about a show or movie (As array). Shows is an array of shows
+// note that for shows, genres is a flat array; for attributes it is an array of objects
+  console.log("filtering by " + JSON.stringify(attributes));
   var filtered = [];
   for (var i = 0; i < shows.length; i++) {
     shows[i].numberOfMatchingAttributes = 0;
-    for (var j = 0; j < shows[i].likes.length; j++) {
+    for (var j = 0; j < shows[i].genres.length; j++) {
       for(var k = 0; k < attributes.length; k++){
-        if(shows[i].likes[j].name.toLowerCase() == attributes[k].name.toLowerCase()) {
+        if(shows[i].genres[j].toLowerCase() == attributes[k].name.toLowerCase()) {
           shows[i].numberOfMatchingAttributes++;
         }
       }
@@ -182,7 +177,8 @@ function narrowByAttributes(attributes, shows){
   return filtered;
 }
 
-function retrieveWatchingRecs(){
+function retrieveWatchingRecs(response){
+    console.log("Retrieving recommendations");
     if(response.session('candidates')){
         //we've already done some work, so use candidates as our basis
         filterlist = JSON.parse(response.session('candidates'));
@@ -190,13 +186,15 @@ function retrieveWatchingRecs(){
     }
     else
     {
+        console.log("Need to open the file");
         filterlist = JSON.parse(fs.readFileSync('watching-04072016-fixed.json','utf-8'));
         return filterlist.recommendations;
     }
 }
 
-function saveFilterList(list){
-    response.session('candidates') = JSON.stringify(list);
+function saveFilterList(list, response){
+    console.log("Saving current list");
+    //response.session('candidates', JSON.stringify(list));
 }
 
 function tryrecommending(filterlist) {
@@ -204,6 +202,7 @@ function tryrecommending(filterlist) {
         //pick a recommendation and give it
         idx = get_random_int (0, num_movies - 1);
         response.say ("I would recommend " + filterlist[idx].title + ". " + filterlist[idx].description);
+        response.shouldEndSession (true);
         response.send ();
     }
     else
